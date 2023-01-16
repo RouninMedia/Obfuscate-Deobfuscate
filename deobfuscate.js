8323ee8c6faafdd6e384267ff974ef979eff301c
Alan Lansdowne <alan_lansdowne@yahoo.com>
2023-01-16 13:05:17 +0100

    Update deobfuscate.js

--- deobfuscate.js
+++ deobfuscate.js
@@ -1,9 +1,9 @@
 function deobfuscate(obfuscatedString)  {
 
-  let blockLength = parseInt(obfuscatedString.substr(-7, 1) + obfuscatedString.substr(6, 1));
+  let blockCount = parseInt(obfuscatedString.substr(-7, 1) + obfuscatedString.substr(6, 1));
   obfuscatedString = obfuscatedString.substr(0, 6) + obfuscatedString.substr(7, (obfuscatedString.length - 14)) + obfuscatedString.substr(obfuscatedString.length - 6);
   
-  var regex = new RegExp('(.{' + (obfuscatedString.length / blockLength) + '})', 'g');
+  var regex = new RegExp('(.{' + (obfuscatedString.length / blockCount) + '})', 'g');
   let obfuscatedArray = obfuscatedString.replace(regex, '$1|').split('|');
   obfuscatedArray.pop();
 
