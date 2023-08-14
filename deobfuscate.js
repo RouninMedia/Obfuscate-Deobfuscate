7e819fc1ad256a9be04d8a41666004f413d86450
Alan Lansdowne <alan_lansdowne@yahoo.com>
2023-08-14 21:51:28 +0100

    Update deobfuscate.js

--- deobfuscate.js
+++ deobfuscate.js
@@ -1,24 +1,33 @@
 function deobfuscate(obfuscatedString)  {
 
-  let blockCount = parseInt(obfuscatedString.substr(-7, 1) + obfuscatedString.substr(6, 1));
-  obfuscatedString = obfuscatedString.substr(0, 6) + obfuscatedString.substr(7, (obfuscatedString.length - 14)) + obfuscatedString.substr(obfuscatedString.length - 6);
+  let stringSegments = [];
+  let obfuscatedStringSegments = obfuscatedString.split('_');
   
-  var regex = new RegExp('(.{' + (obfuscatedString.length / blockCount) + '})', 'g');
-  let obfuscatedArray = obfuscatedString.replace(regex, '$1|').split('|');
-  obfuscatedArray.pop();
+  for (let s = 0; s < obfuscatedStringSegments.length; s++) {
+  	
+    let obfuscatedStringSegment = obfuscatedStringSegments[s];
 
+    let blockCount = parseInt(obfuscatedStringSegment.substr(-7, 1) + obfuscatedStringSegment.substr(6, 1));
+    obfuscatedStringSegment = obfuscatedStringSegment.substr(0, 6) + obfuscatedStringSegment.substr(7, (obfuscatedStringSegment.length - 14)) + obfuscatedStringSegment.substr(obfuscatedStringSegment.length - 6);
+  
+    const regex = new RegExp('(.{' + (obfuscatedStringSegment.length / blockCount) + '})', 'g');
+    let obfuscatedArray = obfuscatedStringSegment.replace(regex, '$1|').split('|');
+    obfuscatedArray.pop();
 
-  while (obfuscatedArray[0].split('').length > 1) {
+    while (obfuscatedArray[0].split('').length > 1) {
 
-    for (let i = 0; i < obfuscatedArray.length; i++) {
+      for (let i = 0; i < obfuscatedArray.length; i++) {
 
-      obfuscatedArray[i] = obfuscatedArray[i].split('').reverse().join('');
-      obfuscatedArray[i] = window.atob(obfuscatedArray[i]);
-      obfuscatedArray[i] = obfuscatedArray[i].replace(/\=/g, '');
+        obfuscatedArray[i] = obfuscatedArray[i].split('').reverse().join('');
+        obfuscatedArray[i] = window.atob(obfuscatedArray[i]);
+        obfuscatedArray[i] = obfuscatedArray[i].replace(/\=/g, '');
+      }
     }
-  }
 
-  let string = obfuscatedArray.join('');
+    let stringSegment = obfuscatedArray.join('');
+    stringSegment = stringSegment.replace(' [(...)]', '');
+    stringSegments[s] = stringSegment;
+  }
   
-  return string;
+  return stringSegments.join('');
 }
